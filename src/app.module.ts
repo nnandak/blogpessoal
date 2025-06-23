348af69a32851388458821aa7c9bda6e4c1b4b8d
nnandak <kannandakarine@gmail.com>
2025-06-23 17:11:52 -0300

    meu primeiro commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { PostagemModule } from './postagem/postagem.module';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [Postagem],
+      synchronize: true,
+      logging: true,
+    }),
+    PostagemModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
